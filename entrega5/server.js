4ea1d7871a07bc37a9046a851fc6856aec34c1dc
Alejandro Cifuentes Aluma <ing.josealejandro@hotmail.com>
2022-03-13 23:19:58 -0500

    add entrega5

---
+++ entrega5/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const productosRoutes = require('./routes/productos');
+const PORT = 8080;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api/productos', productosRoutes);
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+})
+
+server.on("error", error => console.log(`Error in server ${error}`));
